c938c65881d63bc275f24b3f2089d467202841af
dhruv-panchal211 <dhruv.panchal0211@gmail.com>
2023-07-13 09:57:48 +0530

    Create customer route added.

--- src/pages/customer/CustomerList.jsx
+++ src/pages/customer/CustomerList.jsx
@@ -1,14 +1,26 @@
 import { useEffect } from "react";
 import axiosPrivate from "../../api/BaseURL";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const CustomerList = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     axiosPrivate.get(
       `api/resource/Customer/?fields=["name","customer_name","customer_group","customer_type"]`
     );
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="m-6">
+      <div className="flex items-center justify-between">
+        <h3 className="text-white">Customer List</h3>
+        <Button onClick={() => navigate("create")} className="!bg-white">
+          Create
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default CustomerList;

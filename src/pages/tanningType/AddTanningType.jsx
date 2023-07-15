0b8fa74ba2575e9e73a21543205147077649c436
dhruv-panchal211 <dhruv.panchal0211@gmail.com>
2023-07-15 18:32:50 +0530

    add tanning type api

--- src/pages/tanningType/AddTanningType.jsx
+++ src/pages/tanningType/AddTanningType.jsx
@@ -1,33 +1,131 @@
-import { Button, Divider, Typography } from "@mui/material";
+import { useState } from "react";
+import { Button, Divider, TextField, Typography, MenuItem } from "@mui/material";
 import Input from "../../component/common/Input";
+import axiosPrivate from "../../api/BaseURL";
 
 const AddTanningType = () => {
-  return (
-    <div>
-      <div className="flex items-center justify-between mx-3 my-6">
-        <div className="text-white">Add Tanning Type</div>
-        <Button className="!bg-white">Save</Button>
-      </div>
-      <div>
-        <div className="flex gap-4">
-          <Input label="Tanning Type" />
-          <Input label="Default duration (In Minutes)" />
-        </div>
-        <Divider className="!my-6 !border-white" />
-        <div className="my-4">
-          <Typography className="text-white">Item Details</Typography>
-        </div>
-        <div className="flex gap-4">
-          <Input label="Item Code" />
-          <Input label="Rate" />
-        </div>
-        <div className="flex gap-4 my-4">
-          <Input label="Item Name" />
-          <Input label="Item Group" />
-        </div>
-      </div>
-    </div>
-  );
+	const [tanningType, setTanningType] = useState({
+		therapyType: "",
+		duration: "",
+		itemCode: "",
+		rate: "",
+		itemName: "",
+		itemGroup: "",
+	});
+
+	console.log("val", tanningType);
+
+	const tanningTypeForm = [
+		{
+			id: "therapyType",
+			label: "Tanning Type",
+			type: "text",
+		},
+		{
+			id: "duration",
+			label: "Default duration (In Minutes)",
+			type: "text",
+		},
+	];
+
+	const itemDetails = [
+		{
+			id: "itemCode",
+			label: "Item Code",
+			type: "text",
+		},
+		{
+			id: "rate",
+			label: "Rate",
+			type: "text",
+		},
+		{
+			id: "itemName",
+			label: "Item Name",
+			type: "text",
+		},
+	];
+
+	const itemGroup = ["Consumable", "Drug", "Laboratory", "Products"];
+
+	const handleSubmit = () => {
+		axiosPrivate.post("api/resource/Therapy Type", "", {
+			params: {
+				therapy_type: tanningType.therapyType,
+				item: tanningType.itemCode,
+				item_name: tanningType.itemName,
+				item_group: tanningType.itemGroup,
+				rate: tanningType.rate,
+				duration: tanningType.duration,
+			},
+		});
+	};
+
+	return (
+		<div>
+			<div className="flex items-center justify-between mx-3 my-6">
+				<div className="text-white">Add Tanning Type</div>
+				<Button className="!bg-white" onClick={() => handleSubmit()}>
+					Save
+				</Button>
+			</div>
+			<div>
+				<div className="grid grid-cols-2 gap-4">
+					{tanningTypeForm.map((ele) => (
+						<Input
+							className="col-span-1"
+							key={ele.id}
+							label={ele.label}
+							type={ele.type}
+							value={tanningType[ele.id]}
+							setValue={(val) =>
+								setTanningType((prev) => ({ ...prev, [ele.id]: val }))
+							}
+						/>
+					))}
+				</div>
+				<Divider className="!my-6 !border-white" />
+				<div className="my-4">
+					<Typography className="text-white">Item Details</Typography>
+				</div>
+				<div className="grid grid-cols-2 gap-6">
+					{itemDetails.map((ele) => (
+						<Input
+							className="col-span-1"
+							key={ele.id}
+							label={ele.label}
+							type={ele.type}
+							value={tanningType[ele.id]}
+							setValue={(val) =>
+								setTanningType((prev) => ({ ...prev, [ele.id]: val }))
+							}
+						/>
+					))}
+					<TextField
+						fullWidth
+						label="Select blood group"
+						select
+						variant="filled"
+						value={tanningType.itemGroup}
+						onChange={(event) =>
+							setTanningType((prev) => ({
+								...prev,
+								itemGroup: event.target.value,
+							}))
+						}>
+						<MenuItem disabled defaultChecked>
+							All Item Groups
+						</MenuItem>
+						{itemGroup.map((group, index) => (
+							<MenuItem key={index} value={group}>
+								{group}
+							</MenuItem>
+						))}
+					</TextField>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AddTanningType;
